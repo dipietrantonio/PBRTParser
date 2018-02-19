dceb5fc2802238e7b3603ef6e5b42db543f4238e
Halolegend94 <cristiandipietrantonio@gmail.com>
2018-02-19 03:59:14 +0100

    Changes the structure. Parser started.

---
+++ build.bat
@@ -0,0 +1,5 @@
+mkdir build
+cd build
+cmake -G "Visual Studio 15 Win64" ..
+msbuild ALL_BUILD.vcxproj /p:Configuration=Release
+cd ..
--- src/PBRTParser.cpp
+++ src/PBRTParser.cpp
@@ -17,6 +17,7 @@
 #include <sstream>
 #include <exception>
 #include <locale>
+#include "yocto_gl.h"
 
 class InputEndedException : public std::exception {
 
@@ -47,7 +48,7 @@ class SyntaxErrorException : public std::exception{
     }
 };
 
-enum LexemeType {INDENTIFIER, NUMBER, STRING, ARRAY, SINGLETON};
+enum LexemeType {INDENTIFIER, NUMBER, STRING, SINGLETON};
 
 class Lexeme {
     private:
@@ -62,6 +63,10 @@ class Lexeme {
     }
     std::string get_value(){
         return this->value;
+    }
+
+    LexemeType get_type(){
+        return this->type;
     }
 };
 
@@ -89,6 +94,8 @@ class PBRTLexer{
     public:
     PBRTLexer(std::string text);
     bool get_next_lexeme(Lexeme &lex);
+    int get_column(){ return this->column;};
+    int get_line(){ return this->line; };
 };
 
 PBRTLexer::PBRTLexer(std::string text){
@@ -108,8 +115,7 @@ PBRTLexer::PBRTLexer(std::string text){
 // parse
 //
 bool PBRTLexer::get_next_lexeme(Lexeme &lex){
-    try{
-        this->remove_blanks();
+    this->remove_blanks();
 
     if(this->read_indentifier(lex))
         return true;
@@ -119,20 +125,19 @@ bool PBRTLexer::get_next_lexeme(Lexeme &lex){
         return true;
     
     char c = this->look();
-    if(c == '[' || c == ']'){
+    if(c == '[' || c == ']' || c == '\n'){
         std::stringstream ss = std::stringstream();
         ss.put(c);
         lex = Lexeme(LexemeType::SINGLETON, ss.str());
+        if(c == '\n')
+            this->column++;
+        this->advance();
         return true;
     }
 
     std::stringstream errStr;
     errStr << "Lexical error (line " << this->line << ", column " << this->column << ") input not recognized.";
     throw LexicalErrorException(errStr.str());
-
-    }catch(InputEndedException c){
-        return false;
-    }
 }
 
 bool PBRTLexer::read_indentifier(Lexeme &lex){
@@ -254,8 +259,7 @@ void PBRTLexer::advance(){
 // position till the next meaningful character. That is, remove all the characters
 // that are not part of the alphabet.
 //
-void PBRTLexer::remove_blanks(){
-    
+void PBRTLexer::remove_blanks(){ 
     while(true){
         char tmp = this->look();
         if(tmp == ' ' || tmp == '\t'){
@@ -283,12 +287,166 @@ void PBRTLexer::remove_blanks(){
  * PBRTParser
  * Parses a stream of lexemes.
  */
+
+struct PBRTParameter{
+    std::string type;
+    std::string name;
+    void *value;
+};
+
 class PBRTParser {
 
     private:
     PBRTLexer *lexer;
+    void parse_preworld_statements(ygl::scene *scn);
+    void parse_world_statements(ygl::scene *scn);
+    void parse_parameter(PBRTParameter &par, std::string type = "");
+
+    inline void throw_syntax_error(std::string msg){
+        std::stringstream ss;
+        ss << "Syntax Error (line " << this->lexer->get_line() << ", column " << this->lexer->get_column() << "): " << msg;
+        throw SyntaxErrorException(ss.str());
+    };
 
     public:
-    PBRTParser(std::string)
+    PBRTParser(std::string text){
+        this->lexer = new PBRTLexer(text);
+    };
+    void parse(ygl::scene *scn);
 
+};
+
+
+void PBRTParser::parse_preworld_statements(ygl::scene *scn){
+    // TODO
+}
+
+void PBRTParser::parse_world_statements(ygl::scene *scn) {
+	// TODO
+}
+void parse_statement(){
+
+}
+
+std::vector<std::string> split(std::string text){
+
+    std::vector<std::string> results;
+    std::stringstream ss;
+    
+    for (char c : text){
+        if(c == ' '){
+            auto str = ss.str();
+            if(str.size() > 0){
+                results.push_back(str);
+                ss = std::stringstream();
+            }
+        }else{
+            ss << c;
+        }
+    }
+    auto str = ss.str();
+    if(str.length() > 0)
+        results.push_back(str);
+    return results;
+}
+
+bool check_type_existence(std::string &val){
+    if(!val.compare("integer")) return true;
+    if(!val.compare("float")) return true;
+    if(!val.compare("point2")) return true;
+    if(!val.compare("vector2")) return true;
+    if(!val.compare("point3")) return true;
+    if(!val.compare("vector3")) return true;
+    if(!val.compare("normal3")) return true;
+    if(!val.compare("spectrum")) return true;
+    if(!val.compare("bool")) return true;
+    if(!val.compare("string")) return true;
+	if (!val.compare("rgb")) return true;
+	if (!val.compare("color")) return true;
+    if(!val.compare("point")) return true;
+    if(!val.compare("vector")) return true;
+    if(!val.compare("normal")) return true;
+    return false;
+}
+
+void PBRTParser::parse_parameter(PBRTParameter &par, std::string type){
+    
+    Lexeme lex;
+    this->lexer->get_next_lexeme(lex);
+    
+    if(lex.get_type() != LexemeType::STRING)
+        throw_syntax_error("Expected a string of the form \"type name\".");
+    auto tokens = split(lex.get_value());
+    // handle type
+    if(!check_type_existence(tokens[0]))
+        throw_syntax_error("Unrecognized type.");
+    if(!type.compare("") && !(type.compare(tokens[0]))){
+        std::stringstream ss;
+        ss << "Expected parameter of type `" << type << "` but got `" << tokens[0] << "`.";
+        throw_syntax_error(ss.str());
+    }
+    par.type = tokens[0];
+	par.name = tokens[1];
+
+	// now, according to type, we parse the value
+	if (!par.type.compare("string")) {
+		std::vector<std::string> *vals = new std::vector<std::string>();	
+		this->lexer->get_next_lexeme(lex);
+		// it can be a single value or multiple values
+		if (lex.get_type() == LexemeType::STRING) {
+			// single value
+			vals->push_back(lex.get_value());
+		}
+		else if (lex.get_type() == LexemeType::SINGLETON && !lex.get_value().compare("[")) {
+			// start array of value(s)
+			while(true) {
+				this->lexer->get_next_lexeme(lex);
+				if (lex.get_type() == LexemeType::SINGLETON && !lex.get_value().compare("]"))
+					break; // finished to read the array
+				if (lex.get_type() != LexemeType::STRING)
+					throw_syntax_error("One of the values is not a string.");
+				vals->push_back(lex.get_value());
+			}
+		}
+		else {
+			throw_syntax_error("Expected a string value.");
+		}
+		if (vals->size() == 0)
+			throw_syntax_error("No value specified.");
+		par.value = (void *)vals;
+	}
+	else if (!par.type.compare("point") || !par.type.compare("point3") || par.type.compare("normal") || !par.type.compare("normal3")) {
+		std::vector<ygl::vec3f> *vals = new std::vector<ygl::vec3f>();
+		this->lexer->get_next_lexeme(lex);
+		if (lex.get_type() == LexemeType::SINGLETON && !lex.get_value().compare("[")) {
+			// start array of value(s)
+			bool stopped = false;
+			while (true) {
+				ygl::vec3f value;
+				for (int i = 0; i < 3; i++) {
+					this->lexer->get_next_lexeme(lex);
+					if (lex.get_type() == LexemeType::SINGLETON && !lex.get_value().compare("]")) {
+						if (i == 0) {
+							stopped = true;
+							break;
+						}
+						else
+							throw_syntax_error("Too few values specified.");
+					}
+						
+					if (lex.get_type() != LexemeType::NUMBER)
+						throw_syntax_error("One of the values is not a number.");
+					value[i] = atof(lex.get_value().c_str());
+				}
+				if(!stopped)
+					vals->push_back(value);
+			}
+		}
+		else {
+			throw_syntax_error("Expected a string value.");
+		}
+		if (vals->size() == 0)
+			throw_syntax_error("No value specified.");
+		par.value = (void *)vals;
+	}
 }
